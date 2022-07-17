917a84d40c4dd377721414e60f8ae9b7c322cb7c
teru1991 <teltel1991@gmail.com>
2022-07-17 20:50:05 +0900

    Change Title

--- my_portfolio/src/pages/demos/index.jsx
+++ my_portfolio/src/pages/demos/index.jsx
@@ -91,9 +91,9 @@ const Demos = () => {
             <div className="col-lg-12 static text-center">
               <div className="capt mt-50">
                 <div className="parlx">
-                  <h1 className="mb-10">Archo Template</h1>
+                  <h1 className="mb-10">Keiji's Portfolio</h1>
                   <p className="ls10 text-u">
-                    Creative Architecture Interior Template.
+                    My portfolio and self-introduction.
                   </p>
                 </div>
               </div>
